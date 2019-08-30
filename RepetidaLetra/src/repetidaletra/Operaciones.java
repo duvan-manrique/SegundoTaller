f4c75c9268ff4232350a4e6faa0e827221eef6c3
Duvan Canon <lokus.09@gmail.com>
2019-08-30 08:15:57 -0500

    ya

--- RepetidaLetra/src/repetidaletra/Operaciones.java
+++ RepetidaLetra/src/repetidaletra/Operaciones.java
@@ -5,36 +5,41 @@
  */
 package repetidaletra;
 
-import static java.lang.System.out;
 import java.util.Scanner;
-import java.util.Arrays;
 
 /**
  *
  * @author ivan camilo
  */
 public class Operaciones {
-    int contador;
+   
 
     public Operaciones() {
-        ingresar();
+        inicio();
         
     }
-    
-    public void ingresar (){
+        private void inicio() {
+      String oracion =ingresar();
+        validar(oracion);
+       
+    }
+
+    public String ingresar (){
         String oracion ;
         System.out.println("Diguite su frase ");
         Scanner entrada = new Scanner(System.in);
         oracion = entrada.nextLine();
         System.out.println("Su frase es:  "+oracion);  
-        Integer contador=0;
-        boolean bandera= false;
-        char [][] guardarLetra=new char [oracion.length()][2];
+        return  oracion;
+}
+    public void validar(String oracion){
+     
+        int contador=0;
+        int [] guardarLetra=new int [oracion.length()];
         for(int i=0;i<oracion.length();i++){
-            guardarLetra[i][1]='0';
+            guardarLetra[i]=0;      
         }
-        char[] aCaractere=oracion.toCharArray();
-        String sub ="";
+        String sub;
         for (int i = 0; i<oracion.length(); ++i){
           sub=oracion.substring(0,(i+1));
             System.out.println("esta leyendo: "+sub.charAt(i));
@@ -44,28 +49,25 @@ public class Operaciones {
                      contador++; 
                   }
               }
-             
-              guardarLetra[j][0]= sub.charAt(j);
-              guardarLetra[j][1]= contador.toString().charAt(0);
-               contador=0;
+             guardarLetra[j]= contador;
+             contador=0;
           }
+       validar2(i,guardarLetra,sub);
+    }
+}
+
+    private void validar2(int i,int guardarLetra[],String sub) {
+       boolean bandera= false;
           for(int l=0;l<=i;l++){
-              if(guardarLetra[l][1]=='1'){
-                  System.out.println("la que no se repite es: "+guardarLetra[l][0]);
-                  bandera=true;
+              if(guardarLetra[l]==1){
+                  System.out.println("la que no se repite es: "+sub.charAt(l));
+                  bandera=true; 
                   break;
               }
           }
           if(bandera==false){
               System.out.println("no hay ninguna que no se repita");
           }
-        bandera= false;
-        }
-
-        
-        
-       
-        
     }
-    
+
 }
